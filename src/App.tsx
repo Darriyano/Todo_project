21e6e61b54e64f96b8901f22d650cbd5d2e60e70
Дарьяно <dashastepanova1901@mail.ru>
2024-01-02 02:25:06 +0500

    Added TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import TodoList from "./components/TodoList";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const date1 = new Date(2024, 1, 1, 12, 0);
+const date2 = new Date(2025, 1, 1, 21, 30)
+
+const initialData = [{
+    title: 'Learn React!',
+    desc: 'Learn faster, you!',
+    image: '',
+    done: true,
+    createdAt: date1.toLocaleString(),
+    key: date1.getTime()
+}]
+
+class App extends Component {
+    data: any;
+    constructor(props: any) {
+        super(props);
+        this.data = initialData;
+    }
+
+    render() {
+        // Todo create CSS for styles of NAV and each DIV
+       return (
+        <div>
+            <nav className="">
+                <div className="">
+                    <span className="">
+                        What to do now!
+                    </span>
+                </div>
+            </nav>
+            <main className="content px-6 mt-6">
+                <TodoList list={this.data}></TodoList>
+            </main>
+        </div>
+       );
+    }
 }
 
 export default App;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,5 @@
+export default  function TodoList(props: any) {
+    return(
+        <div>Here will be actual tasks!</div>
+    );
+}
